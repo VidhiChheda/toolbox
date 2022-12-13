d5e1b8a80cf46e4cfe8686c606b41a7ca4f15ce1
VidhiChheda <67408078+VidhiChheda@users.noreply.github.com>
2022-12-13 14:39:38 +0530

    Add files via upload

---
+++ toolbox/Hello.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 13 10:25:48 2022
+
+@author: admin
+"""
+
+def hellofunc():
+    print("Hello my dear friend. Thanks for calling me.")
---
+++ toolbox/Whatsup.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 13 10:26:34 2022
+
+@author: admin
+"""
+
+def askWellBeing():
+    print("How are you my friend? I hope you are doing great in your life.")
